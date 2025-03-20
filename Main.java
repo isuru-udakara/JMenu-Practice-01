c100b50628a70205da2d57a524c0c0c902ccabbc
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-21 00:40:24 +0530

    Imported JMenubar and created a varible for it

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
+import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.Color;
 
 public class Main{
     JFrame window;
+    JMenuBar menuBar;
 
     public Main(){
         CreateWindow();
