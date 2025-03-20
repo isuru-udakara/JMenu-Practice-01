0c59c29d4df8c75ae570a4ad1ab6a1da9d5c176c
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-03-21 00:36:33 +0530

    Created parent window and customized

--- Main.java
+++ Main.java
@@ -8,7 +8,22 @@ public class Main{
     JFrame window;
 
     public Main(){
-        
+        CreateWindow();
+
+        // make visible all components 
+        window.setVisible(true);
+    }
+
+    private void CreateWindow(){
+        window= new JFrame();
+        window.setTitle("JMenu Practice");
+        window.setResizable(true);
+        window.setSize(800, 600);
+        window.setLocationRelativeTo(null);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(new Color(100, 200, 150));
+        ImageIcon icon= new ImageIcon("icon.png");
+        window.setIconImage(icon.getImage());
     }
 
     public static void main(String args[]){
